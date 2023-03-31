d85e83aee4e87ea0bfc2ace87d16934528a62a2c
Nizam <ramazanov570633@gmail.com>
2023-03-31 21:57:27 +0400

    socketio8

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }))
 mongoose.connect(CONNECTION_STRING)
 .then(res=>console.log('connect'))
 .catch(err=>console.log(err))
-// app.options("*", cors({ origin: ['http://localhost:19008', 'http://localhost:8082'], optionsSuccessStatus: 200 }));
-app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
+app.options("*", cors({ origin: ['http://localhost:19006', 'http://localhost:8082'], optionsSuccessStatus: 200 }));
+// app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
