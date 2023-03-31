f3ca7d2008d8e7f509802a3ed7c733962fe3c592
Nizam <ramazanov570633@gmail.com>
2023-03-31 23:06:28 +0400

    socketio12

--- src/app.js
+++ src/app.js
@@ -14,15 +14,20 @@ app.use(express.urlencoded({ extended: true }))
 mongoose.connect(CONNECTION_STRING)
 .then(res=>console.log('connect'))
 .catch(err=>console.log(err))
-app.options("*", cors({ origin: ['http://localhost:19006', 'http://localhost:8082', 'https://bankapi-2puz.onrender.com'], optionsSuccessStatus: 200 }));
-app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors({
+  origin: ['http://localhost:19006', 'http://localhost:8082', 'https://bankapi-2puz.onrender.com'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'X-HTTP-Method-Override', 'Content-Type', 'Accept'],
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   next();
+// });
   
 app.use('/api/user',userRouter);
 app.use('/api/new',newsrouter);
