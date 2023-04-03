7667bf179a0de82bd8102a31bc43309c2ed9478a
Nizam <ramazanov570633@gmail.com>
2023-04-03 14:08:37 +0400

    socketio22

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const userRouter = require('./api/routers/user');
 const User = require('./models/user');
 const newsrouter =require('./api/routers/newsrouter')  
-app.use(cors())
+app.use(cors({origin: '*'}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 mongoose.connect(CONNECTION_STRING)
@@ -17,7 +17,7 @@ mongoose.connect(CONNECTION_STRING)
 // app.options("*", cors({ origin: ['http://localhost:19007', 'http://localhost:8082', 'https://bankapi-2puz.onrender.com'], optionsSuccessStatus: 200 }));
 // app.options('*', cors());
 
-app.options("*", cors({ origin: '*', optionsSuccessStatus: 200 }));
+// app.options("*", cors({ origin: '*', optionsSuccessStatus: 200 }));
 
 app.use('/api/user',userRouter);
 app.use('/api/new',newsrouter);
