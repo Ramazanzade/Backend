032487167151777b5c38deb6bf0b1efd2242e38f
Nizam <ramazanov570633@gmail.com>
2023-04-03 16:11:24 +0400

    socketio28

--- src/server.js
+++ src/server.js
@@ -4,21 +4,6 @@
     require('dotenv').config();
     const socketio = require('socket.io')
     const server = http.createServer(app)
-    const { Server } = require("socket.io");
-    const io = new Server(server);   
-    io.on('connection', (socket) => {
-        console.log('a user connected');
-      
-      
-        socket.on('send_message',(data)=>{
-          console.log("received message in server side",data)
-          io.emit('received_message',data)
-        })
-      
-        socket.on('disconnect', () => {
-          console.log('user disconnected');
-        });
-        
-      });
+
     server.listen(8082)
 
