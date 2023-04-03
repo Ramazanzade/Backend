8bed4e8506511dbf98fb6a24c0dbe0d79417d1e3
Nizam <ramazanov570633@gmail.com>
2023-04-03 16:19:13 +0400

    socketio29

--- src/server.js
+++ src/server.js
@@ -4,6 +4,27 @@
     require('dotenv').config();
     const socketio = require('socket.io')
     const server = http.createServer(app)
+    const { Server } = require("socket.io");
 
+
+    const io = new Server(server, {
+        cors: {
+          origin: "*",
+          methods: ["GET", "POST"],
+        },
+      });
+      
+      io.on("connection", (socket) => {
+        console.log(`User Connected: ${socket.id}`);
+      
+        socket.on("join_room", (data) => {
+          socket.join(data);
+        });
+      
+        socket.on("send_message", (data) => {
+          socket.to(data.room).emit("receive_message", data);
+        });
+      });
+      
     server.listen(8082)
 
