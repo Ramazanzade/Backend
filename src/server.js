2dd8f69eedfa63ad65092a0a36396df27a3b5711
Nizam <ramazanov570633@gmail.com>
2023-04-04 13:53:37 +0400

    socketi40

--- src/server.js
+++ src/server.js
@@ -1,27 +1,10 @@
     const http =require('http')
-    const PORT= 8082
+    // const PORT= 8082
     const app =require('./app')
     require('dotenv').config();
     // const socketio = require('socket.io')
     const server = http.createServer(app)
-    // const io = require('socket.io')(server, {
-    //   cors: {
-    //     origin: '*',
-    //   }
-    // });    
-
-    // io.on('connection', (socket) => {
-    //   console.log('A user connected');
-    
-    //   socket.on('message', (data) => {
-    //     console.log('Message received: ' + data.message);
-    //     io.emit('message', data);
-    //   });
-    
-    //   socket.on('disconnect', () => {
-    //     console.log('A user disconnected');
-    //   });
-    // });
+  
       
-    server.listen(8082)
+    server.listen(8083)
 
